ad4058695a9068bf8480ac83e20385e470bf5405
Julio Lima <lima.julio.cs@gmail.com>
2025-02-26 09:27:50 -0300

    doc: swagger implementation and configuration

--- src/main/java/br/com/nlw/events/controller/EventController.java
+++ src/main/java/br/com/nlw/events/controller/EventController.java
@@ -2,12 +2,15 @@ package br.com.nlw.events.controller;
 
 import br.com.nlw.events.model.Event;
 import br.com.nlw.events.service.EventService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
+@Tag(name = "Event", description = "API NLW Connect - Event")
 public class EventController {
 
     private final EventService eventService;
--- src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -4,12 +4,18 @@ import br.com.nlw.events.dto.SubscriptionRankingItem;
 import br.com.nlw.events.dto.SubscriptionResponse;
 import br.com.nlw.events.model.User;
 import br.com.nlw.events.service.SubscriptionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
+@Tag(name = "Subscriptions", description = "API NLW Connect - Subscriptions")
 public class SubscriptionController {
 
     private final SubscriptionService subscriptionService;
@@ -18,6 +24,14 @@ public class SubscriptionController {
         this.subscriptionService = subscriptionService;
     }
 
+    @Operation(summary = "Subscribe to event", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Event registration created successfully"),
+            @ApiResponse(responseCode = "404", description = "Event pretty name or user ID not found"),
+            @ApiResponse(responseCode = "422", description = "Invalid parameters request"),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters"),
+            @ApiResponse(responseCode = "500", description = "Service failure"),
+    })
     @PostMapping({"/subscription/{prettyName}", "/subscription/{prettyName}/{userId}"})
     public ResponseEntity<SubscriptionResponse> createSubscription(@PathVariable String prettyName,
                                                                    @RequestBody User subscriber,
@@ -26,6 +40,14 @@ public class SubscriptionController {
         return ResponseEntity.ok().body(result);
     }
 
+    @Operation(summary = "Get ranking", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search successful"),
+            @ApiResponse(responseCode = "404", description = "Event not found"),
+            @ApiResponse(responseCode = "422", description = "Invalid parameters request"),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters"),
+            @ApiResponse(responseCode = "500", description = "Service failure"),
+    })
     @GetMapping("/subscription/{prettyName}/ranking")
     public ResponseEntity<List<SubscriptionRankingItem>> getSubscriptionRanking(@PathVariable String prettyName) {
         List<SubscriptionRankingItem> ranking = subscriptionService.getCompleteRanking(prettyName);
@@ -33,8 +55,16 @@ public class SubscriptionController {
         return ResponseEntity.ok().body(ranking.subList(0, 3));
     }
 
+    @Operation(summary = "Get subscriber ranking position", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search successful"),
+            @ApiResponse(responseCode = "404", description = "Event or user ID not found"),
+            @ApiResponse(responseCode = "422", description = "Invalid parameters request"),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters"),
+            @ApiResponse(responseCode = "500", description = "Service failure"),
+    })
     @GetMapping("/subscription/{prettyName}/ranking/{userId}")
-    public ResponseEntity<?> generateRaningByEventAndUser(@PathVariable String prettyName, @PathVariable Integer userId){
+    public ResponseEntity<?> generateRankingByEventAndUser(@PathVariable String prettyName, @PathVariable Integer userId){
         return ResponseEntity.ok(subscriptionService.getRankingByUser(prettyName, userId));
     }
 }
